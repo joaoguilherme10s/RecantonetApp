1d10981b3ba3dedf9d1f6d0af4e815f121ce7eee
joaoguilherme10s <joaoguilherme2002jg@gmail.com>
2026-02-16 20:51:01 -0300

    Update _layout.tsx

--- recantonetapp/app/_layout.tsx
+++ recantonetapp/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
+      <收获.Screen name="index" />
       <Stack.Screen name="dashboard" />
     </Stack>
   );
